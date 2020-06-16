4787097ed16e176a056d8e2a6a8f9a30fa1cfecd
Nacxit <nacxitArmando@hotmail.com>
2020-06-16 10:45:51 -0600

    Create Header Component

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,4 +1,16 @@
 <script src="./Header.js"></script>
+<style src="./Header.css" module></style>
+
 <template>
-  <div></div>
+  <div>
+    <b-navbar toggleable="lg" :class="[$style.background]">
+      <b-navbar-brand href="#">APP CRUD</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#">Register</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
